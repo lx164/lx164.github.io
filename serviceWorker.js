e8e3da566593452b877e38df404f4d9b1449daf6
lx164 <>
2021-06-24 13:32:13 +0000

    update

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const cacheName = "watermark"; // cache名, 在控制台Application的CaChe下可以看到
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+    }),
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      }),
+  );
+});
